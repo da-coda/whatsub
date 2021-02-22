d12d4f77edb03ea6dcf74e888933c8a8554fb429
Daniel Röhrig <mail@danielroehrig.de>
2021-02-22 22:14:02 +0100

    Remove webserver functionality beyond the websocket

--- whatsub.go
+++ whatsub.go
@@ -21,7 +21,6 @@ func main() {
 	router.HandleFunc("/game/create", CreateGameHandler(gm))
 	router.HandleFunc("/game/{uuid_or_key}/join", gm.JoinGame).Queries("name", "{name}")
 	router.HandleFunc("/game/{uuid}/start", gm.StartGame)
-	router.PathPrefix("/").HandlerFunc(ServeWebpage).Methods("GET")
 	srv := &http.Server{
 		Handler:      router,
 		Addr:         "0.0.0.0:8000",
@@ -49,25 +48,4 @@ func CreateGameHandler(gm *gameLogic.GameMaster) func(writer http.ResponseWriter
 			return
 		}
 	}
-}
-
-func ServeWebpage(w http.ResponseWriter, r *http.Request) {
-	path, err := filepath.Abs(r.URL.Path)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	path = filepath.Join("src/", path)
-
-	_, err = os.Stat(path)
-	if os.IsNotExist(err) {
-		http.ServeFile(w, r, "src/index.html")
-		return
-	} else if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	http.FileServer(http.Dir("src")).ServeHTTP(w, r)
 }
