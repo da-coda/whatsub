ce8a21d72d7fbbd46c6b0968201365b9f4a9c2c3
Daniel Röhrig <mail@danielroehrig.de>
2021-02-23 22:00:24 +0100

    Cleaning up before burning it down

---
+++ web/src/components/StartScreen.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h1>WhatSub?</h1>
+    <button type="button">Start new Game</button>
+    <button type="button">Join Lobby</button>
+  </section>
+</template>
+<script>
+export default {
+  name: 'StartScreen',
+}
+</script>
