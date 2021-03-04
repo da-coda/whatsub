9f65b9734d40f72de4d8b3ce9bd20e84c0c7db81
Daniel Röhrig <mail@danielroehrig.de>
2021-03-04 22:05:08 +0100

    Add Logo

---
+++ web/src/views/BigLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img
+      src="../../public/img/logo.png"
+      width="100"
+    >
+    <h1 class="headline">
+      WhatSub?
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'BigLogo'
+}
+</script>
+<style scoped>
+h1.headline {
+  font-size: 48px;
+}
+</style>
