b36e51d61ed97693729d09287ccd68ad2784dc74
Daniel Röhrig <mail@danielroehrig.de>
2021-03-19 22:05:10 +0100

    Display raw HTML

--- web/src/components/game/HtmlPost.vue
+++ web/src/components/game/HtmlPost.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>{{ round.Content }}</div>
+  <div v-html="htmlContent" />
 </template>
 
 <script>
@@ -9,6 +9,18 @@ export default {
     round: {
       type: Object,
       default: null
+    }
+  },
+  computed: {
+    /**
+     * Fetch the html content from the payload and decodes it to
+     * raw HTML
+     * //TODO This might be a security risk
+     * @return {string} Raw HTML that has been decoded
+     */
+    htmlContent () {
+      const doc = new DOMParser().parseFromString(this.round.Content, 'text/html')
+      return doc.documentElement.textContent
     }
   }
 }
